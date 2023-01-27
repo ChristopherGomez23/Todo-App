28e1f4a9ba26ab189a46bc21f8b18cbce5134956
codeit-chris <gomez.cneil@gmail.com>
2023-01-27 08:04:03 +0800

    router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -9,13 +9,23 @@ import {MatTabsModule} from '@angular/material/tabs';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+
+// const appRoutes: Routes = [
+//     {path: '', component: LoginComponent},
+//   {path: '/signup', component: SignupComponent}
+// .forRoot(appRoutes, { enableTracing : true })]
+
+
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatCardModule,
